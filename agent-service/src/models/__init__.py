01822b8d15ea6a0c0209b18d554771ba397913bc
truong51972 <tranquoctruong20@gmail.com>
2025-11-29 12:20:02 +0700

    add into registry

--- agent-service/src/models/__init__.py
+++ agent-service/src/models/__init__.py
@@ -2,3 +2,5 @@
 
 from src.models.agent.docs_preprocessing_state_model import DocsPreProcessingStateModel
 from src.models.agent.testcases_gen_state_model import TestcasesGenStateModel
+
+from .api.standard_output import StandardOutputModel
